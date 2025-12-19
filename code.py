d4ff3ec7d3fdf4c744b721181b536dc9059f7513
Bestin Binu Mathew <bestin.mathew@stu.ocsb.ca>
2025-12-19 19:20:53 +0000

    finished

--- code.py
+++ code.py
@@ -1,38 +1,60 @@
 #!/usr/bin/env python3
-# Created by: Mr. Mathew
-# Created on: dec 2025
-# This program displays a background and a sprite for the "Space Aliens" game
+
+# Created by: [Your Name]
+# Created on: Dec 2025
+# This program handles button input for movement and printing to the console
 
 import ugame
 import stage
 
 def game_scene():
-    # 1. Load image banks (CITE: Must be 16-color indexed BMPs)
+    # Load image banks (Must be 16-color indexed BMPs)
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # 2. Setup background grid (10x8 tiles)
+    # Setup background and ship sprite
     background = stage.Grid(image_bank_background, 10, 8)
-
-    # 3. Create the ship sprite 
-    # Change '5' to '0' if your ship is the first image in your BMP
     ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-    # 4. Create the stage and set layers
-    # Layers are a list; the ship comes first so it is on top
+    # Create the stage and layers
     game = stage.Stage(ugame.display, 60)
     game.layers = [ship] + [background]
-
-    # 5. Render the background once
     game.render_block()
 
-    # 6. Main game loop
+    # repeat forever, game loop
     while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        # Check for A button (often K_X in some versions)
+        if keys & ugame.K_X:
+            print("A")
         
-        # Redraw the sprite layer
+        # Check for B button (often K_O in some versions)
+        if keys & ugame.K_O:
+            print("B")
+
+        if keys & ugame.K_START:
+            print("Start")
+
+        if keys & ugame.K_SELECT:
+            print("Select")
+
+        # Movement logic using bitwise AND
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+        # Update sprite position and wait for next frame
         game.render_sprites([ship])
-        
-        # Refresh the screen at 60 FPS
         game.tick()
 
 if __name__ == "__main__":
